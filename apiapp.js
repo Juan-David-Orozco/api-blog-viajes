1f7b8c253c8ef76fb4cfb04b24f28feb4748841b
Juan-David-Orozco <ing.ele2018@gmail.com>
2021-12-09 11:12:18 -0500

    endpoints autores

--- apiapp.js
+++ apiapp.js
@@ -67,6 +67,45 @@ app.get('/api/v1/publicaciones/:id', function(peticion, respuesta) {
   
 })
 
+app.get('/api/v1/autores', function(peticion, respuesta) {
+  
+  pool.getConnection(function(err, connection) {
+    const query = `SELECT email, pseudonimo FROM autores`
+    connection.query(query, function(error, filas, campos) {
+      if(respuesta.status(200)){
+        respuesta.json({data: filas})
+      }
+      else{
+        respuesta.send({errors: ["No se obtuvo ningun elemento"]})
+      }
+    })
+    connection.release()
+  })
+  
+})
+
+app.get('/api/v1/autores/:id', function(peticion, respuesta) {
+  
+  pool.getConnection(function(err, connection) {
+    const query = `
+      SELECT autores.id id, email, pseudonimo, publicaciones.id publicacion_id, titulo
+      FROM autores
+      INNER JOIN
+      publicaciones ON
+      autores.id = publicaciones.autor_id
+      WHERE autores.id = ${connection.escape(peticion.params.id)}
+      ORDER BY autores.id DESC, publicaciones.fecha_hora DESC
+    `
+    connection.query(query, function(error, filas, campos) {
+      
+        respuesta.json({data: filas})
+      
+    })
+    connection.release()
+  })
+  
+})
+
 app.listen(8080, function(){
   console.log("Servidor iniciado");
 })

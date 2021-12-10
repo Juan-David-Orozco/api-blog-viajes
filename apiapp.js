a2f706199fc366bf5865edbf5f0137efc12826bd
Juan-David-Orozco <ing.ele2018@gmail.com>
2021-12-09 22:48:49 -0500

    proyecto v1.0.0

--- apiapp.js
+++ apiapp.js
@@ -129,6 +129,23 @@ app.get('/api/v1/autores/:id', function(peticion, respuesta) {
   
 })
 
+app.post('/api/v1/autores/', function(peticion, respuesta) {
+  
+  pool.getConnection(function(err, connection) {
+    const query = `INSERT INTO tareas (descripcion) VALUES (${connection.escape(peticion.body.descripcion)})`
+    connection.query(query, function(error, filas, campos) {
+      const nuevoId = filas.insertId
+      const queryConsulta = `SELECT * FROM tareas WHERE id = ${connection.escape(nuevoId)}`
+      connection.query(queryConsulta, function(error, filas, campos) {
+        respuesta.status(201)
+        respuesta.json({data: filas[0]})
+      })
+    })
+    connection.release()
+  })
+  
+})
+
 app.listen(8080, function(){
   console.log("Servidor iniciado");
 })
